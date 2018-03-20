6c020e79465609df99dcb102f2dee787ffce5ee8
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-03-20 21:02:47 +0700

    Add heartstone card from API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-class App extends Component {
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: [],
+  }
+
+  componentDidMount() {
+    const config = {
+      headers: {
+        'X-Mashape-Key': 'lN2bpG1T3RmshWn88IrodpWtsa0Qp1GGyGsjsnNEVMi5LyyOG7',
+        'Accept': 'application/json'
+      }
+    }
+
+    axios.get(`https://omgvamp-hearthstone-v1.p.mashape.com/cards/sets/Classic`, config)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons: persons });
+        console.log(persons);
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div>
+      <ul>
+        {this.state.persons.map(person =>
+          <div>
+            <img className='rotateRightImg' src='images/cardBack.gif' />
+            <img className='rotateLeftImg' src={person.img} key={person.cardId}/>
+            <p>{person.name}</p>
+          </div>
+        )}
+      </ul>
+    </div>
+    )
   }
 }
-
-export default App;
